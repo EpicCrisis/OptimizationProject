73216d9acd9cdd176ad53c4ad0edaf03a1525163
TerrenceChan <roxes2112@yahoo.com>
2017-11-02 18:36:08 +0800

    *Project to test game optimization.

---
+++ GameOptimization/Assets/Garbage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Garbage : MonoBehaviour
+{
+	//	void Start ()
+	//	{
+	//		//Application.targetFrameRate = 30;
+	//
+	//	}
+	//
+	//	void Update ()
+	//	{
+	//		Transform[] tArray = GameObject.FindObjectsOfType<Transform> ();
+	//
+	//		foreach (Transform t in tArray) {
+	//
+	//			Debug.Log(string.Format("{0}", t.name);
+	//
+	//		}
+	//	}
+}
+
---
+++ GameOptimization/Assets/TransformOptimizationScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformOptimizationScript : MonoBehaviour
+{
+	//	public Transform _transform;
+	//	public Renderer _rend;
+	//
+	//	void Start ()
+	//	{
+	//		//Caching
+	//		this._transform = this.GetComponent<Transform> ();
+	//		this._rend = this.GetComponent<Renderer> ();
+	//	}
+	//
+	//
+	//	void Update ()
+	//	{
+	//		if (posX != 1f) { //Call function when necessary only.
+	//
+	//			RandomFunction ();
+	//		}
+	//
+	//		List<GameObject> items = new List<GameObject> ();
+	//		items.Clear ();
+	//
+	//		Debug.Log (string.Format ("What is this? Optimizing heap. {0}", gameObject.name)); //Boxing, don't do this.
+	//	}
+	//
+	//	IEnumerator Sequence ()
+	//	{
+	//	float delay = 1f;
+	//
+	//	yield return delay;
+	//	yield return new WaitForSeconds(1) //Don't do this!
+	//	}
+}
